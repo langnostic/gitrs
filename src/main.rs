c184de982a27249544d65c157799531c9084b552
langnostic <jfitzy1321@pm.me>
2021-02-24 16:00:15 -0700

    added basic clap cli

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-fn main() {
-    print_help();
-}
+extern crate clap;
 
-fn print_help() {
-    println!("\
-Hello World!
-This is a multi-line string.
-");
+use clap::{crate_version, App, AppSettings, Arg};
+
+fn main() {
+    let _ = App::new("gitrs - git written in rust")
+        .version(crate_version!())
+        .arg(Arg::with_name("init"))
+        .settings(&[AppSettings::ArgRequiredElseHelp])
+        .get_matches();
 }

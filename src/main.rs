e6ec97e8420476fbf3208583d2be540be0a88c68
langnostic <jfitzy1321@pm.me>
2021-02-24 18:15:45 -0700

    creating initial structure for init command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 extern crate clap;
+mod init;
+mod repository;
 
 use clap::{crate_version, App, AppSettings, Arg};
+use repository::Repository;
 
+// Lets start by recreating the 'init' command
+// I need to create a '.git' folder at the given directory
+// what should happen if .git already exists?
 fn main() {
-    let _ = App::new("gitrs - git written in rust")
+    let matches = App::new("gitrs - git written in rust")
         .version(crate_version!())
         .arg(Arg::with_name("init"))
         .settings(&[AppSettings::ArgRequiredElseHelp])
         .get_matches();
+
+    let main_result: Result<(), ()>;
+
+    if matches.is_present("init") {
+        main_result = init::run_init();
+    }
+
+    let _ = Repository {};
 }

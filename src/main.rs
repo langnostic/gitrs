f1be02ac42ebae06547acdfaa030eb9d09bd7aab
langnostic <langnostic1@pm.me>
2021-05-31 02:12:22 -0600

    considering my options for cli parsers

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,102 @@
-static DEFAULT_GIT_DIR: &'static str = ".gitrs";
+mod args;
+
+use args::GitArgs;
+use structopt::StructOpt;
+
+// static DEFAULT_GIT_DIR: &'static str = ".gitrs";
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() == 1 {
-        print_short_help();
+    let args: GitArgs = GitArgs::from_args();
+
+    // I need to find a list of all available git commands,
+    // subcommands, plumbing and porcelian.
+
+    println!("{:#?}", args);
+    if args.version {
+        let version = option_env!("CARGO_PKG_VERSION").unwrap();
+        println!("version: {}", version);
         return;
     }
 
-    match args[1].as_str() {
-        "init" => commands::init::main(&args[2..]),
-        "help" => print_full_help(),
-        _ => {
-            print_short_help();
-            println!("Command not support!");
-        }
-    };
+    // if let Some(_) = args.init {
+    //     objects::Repository::new(DEFAULT_GIT_DIR);
+    // }
+
+    // match args[1].as_str() {
+    //     "init" => commands::init::main(&args[2..]),
+    //     "help" => print_full_help(),
+    //     _ => {
+    //         print_short_help();
+    //         println!("Command not support!");
+    //     }
+    // };
 }
 
-fn print_short_help() {
-    println!(
-        "\
-    This is the help menu"
-    );
-}
+// fn print_short_help() {
+//     println!(
+//         "\
+//     This is the help menu"
+//     );
+// }
 
-fn print_full_help() {
-    println!("");
-}
+// fn print_full_help() {
+//     println!("");
+// }
 
-mod commands {
-    pub mod init {
-        pub fn main(args: &[String]) {
-            let dir = match args.len() {
-                0 => ".",
-                _ => args[0].as_str(),
-            };
+// mod objects {
+//     use super::DEFAULT_GIT_DIR;
+//     use path_clean::PathClean;
+//     use std::fs;
+//     use std::io;
+//     use std::path::PathBuf;
 
-            let repo = crate::objects::Repository::new(dir);
+//     // TODO: Question for future langnostic
+//     // Can I replicate this folder structure as a struct?
+//     const DEFAULT_GIT_DIR_TREE: &[&str] = &["objects", "refs", "refs/heads"];
 
-            println!(
-                "Initialized empty Gitrs repository in {}",
-                repo.path.display()
-            );
-        }
-    }
-}
+//     // TODO: consider moving this to a template folder?
+//     // const DEFAULT_LOCAL_CONFIG: &str = "\
+//     // [core]
+//     // 	repositoryformatversion = 0
+//     // 	filemode = true
+//     // 	bare = false
+//     // 	logallrefupdates = true
+//     // ";
 
-mod objects {
-    use super::DEFAULT_GIT_DIR;
-    use path_clean::PathClean;
-    use std::fs;
-    use std::io;
-    use std::path::PathBuf;
+//     pub struct Repository {
+//         pub path: PathBuf,
+//     }
 
-    // TODO: Question for future langnostic
-    // Can I replicate this folder structure as a struct?
-    const DEFAULT_GIT_DIR_TREE: &[&str] = &["objects", "refs", "refs/heads"];
+//     impl Repository {
+//         pub fn new(dir: &str) -> Self {
+//             let path = match dir {
+//                 "." | "./" => std::env::current_dir().unwrap(),
+//                 _ => PathBuf::from(dir).clean(),
+//             }
+//             .join(DEFAULT_GIT_DIR);
 
-    // TODO: consider moving this to a template folder?
-    // const DEFAULT_LOCAL_CONFIG: &str = "\
-    // [core]
-    // 	repositoryformatversion = 0
-    // 	filemode = true
-    // 	bare = false
-    // 	logallrefupdates = true
-    // ";
+//             // TODO: what does init reinitialize actually do?
+//             if !path.exists() {
+//                 Self::create_repo(&path).unwrap();
+//             }
 
-    pub struct Repository {
-        pub path: PathBuf,
-    }
+//             Self { path }
+//         }
 
-    impl Repository {
-        pub fn new(dir: &str) -> Self {
-            let path = match dir {
-                "." | "./" => std::env::current_dir().unwrap(),
-                _ => PathBuf::from(dir).clean(),
-            }
-            .join(DEFAULT_GIT_DIR);
+//         fn create_repo(git_path: &PathBuf) -> io::Result<()> {
+//             for dir in DEFAULT_GIT_DIR_TREE.iter() {
+//                 fs::create_dir_all(git_path.join(dir))?;
+//             }
 
-            // TODO: what does init reinitialize actually do?
-            if !path.exists() {
-                Self::create_repo(&path).unwrap();
-            }
+//             fs::write(git_path.join("HEAD"), "refs: refs/heads/main\n")?;
+//             Ok(())
+//         }
+//     }
+//     // pub struct Commit {
+//     //     parent: Option<Box<Commit>>,
+//     // }
 
-            Self { path }
-        }
+//     // struct Blob {}
 
-        fn create_repo(git_path: &PathBuf) -> io::Result<()> {
-            for dir in DEFAULT_GIT_DIR_TREE.iter() {
-                fs::create_dir_all(git_path.join(dir))?;
-            }
-
-            fs::write(git_path.join("HEAD"), "refs: refs/heads/main\n")?;
-            Ok(())
-        }
-    }
-    pub struct Commit {
-        parent: Option<Box<Commit>>,
-    }
-
-    struct Blob {}
-
-    struct Tree {}
-}
+//     // struct Tree {}
+// }

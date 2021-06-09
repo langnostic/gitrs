6c0b837cad07a5390ffa025ede0b06ff28f20dd7
langnostic <langnostic1@pm.me>
2021-06-08 22:36:44 -0600

    added init mod and repo back into main, to perserve to progress I already made with init command

--- src/main.rs
+++ src/main.rs
@@ -51,3 +51,70 @@ to read about a specific subcommands or concept.
 See \"git help git\" for an overview of the system"
     );
 }
+
+mod init {
+    pub fn main(args: &[String]) {
+        let dir = match args.len() {
+            0 => ".",
+            _ => args[0].as_str(),
+        };
+
+        let repo = super::repo::Repository::new(dir);
+
+        println!(
+            "Initialized empty Gitrs repository in {}",
+            repo.path.display()
+        );
+    }
+}
+
+mod repo {
+    use path_clean::PathClean;
+    use std::fs;
+    use std::io;
+    use std::path::PathBuf;
+
+    const DEFAULT_GIT_DIR: &'static str = ".gitrs";
+    // TODO: Question for future langnostic
+    // Can I replicate this folder structure as a struct?
+    const DEFAULT_GIT_DIR_TREE: &[&str] = &["objects", "refs", "refs/heads"];
+
+    // TODO: consider moving this to a template folder?
+    // const DEFAULT_LOCAL_CONFIG: &str = "\
+    // [core]
+    // 	repositoryformatversion = 0
+    // 	filemode = true
+    // 	bare = false
+    // 	logallrefupdates = true
+    // ";
+
+    pub struct Repository {
+        pub path: PathBuf,
+    }
+
+    impl Repository {
+        pub fn new(dir: &str) -> Self {
+            let path = match dir {
+                "." | "./" => std::env::current_dir().unwrap(),
+                _ => PathBuf::from(dir).clean(),
+            }
+            .join(DEFAULT_GIT_DIR);
+
+            // TODO: what does init reinitialize actually do?
+            if !path.exists() {
+                Self::create_repo(&path).unwrap();
+            }
+
+            Self { path }
+        }
+
+        fn create_repo(git_path: &PathBuf) -> io::Result<()> {
+            for dir in DEFAULT_GIT_DIR_TREE.iter() {
+                fs::create_dir_all(git_path.join(dir))?;
+            }
+
+            fs::write(git_path.join("HEAD"), "refs: refs/heads/main\n")?;
+            Ok(())
+        }
+    }
+}

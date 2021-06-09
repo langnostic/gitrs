1af0af8d16c44e296b8468c5da0789aa4195f674
langnostic <langnostic1@pm.me>
2021-06-08 22:23:25 -0600

    removing structopts and commented out code

---
+++ src/builtins.rs
@@ -0,0 +1,6 @@
+pub fn git_status_main(args: GitArgs) {
+    // construct options for this command
+
+    // check if first (second?) arg is '-h'
+    // exit(129) ?? what is this status code ??
+}
--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,53 @@
-mod args;
-
-use args::GitArgs;
-use structopt::StructOpt;
-
-// static DEFAULT_GIT_DIR: &'static str = ".gitrs";
-
 fn main() {
-    let args: GitArgs = GitArgs::from_args();
+    // Shit, structops automatically creates a -h option
+    // I might have to bite the bullet and manually manage args
+    // let args: GitArgs = GitArgs::from_args();
+
+    let args: Vec<String> = std::env::args().collect();
 
     // I need to find a list of all available git commands,
     // subcommands, plumbing and porcelian.
 
-    println!("{:#?}", args);
-    if args.version {
-        let version = option_env!("CARGO_PKG_VERSION").unwrap();
-        println!("version: {}", version);
-        return;
+    println!("number of args: {}", args.len());
+    for arg in std::env::args() {
+        println!("{:?}", arg);
     }
 
-    // if let Some(_) = args.init {
-    //     objects::Repository::new(DEFAULT_GIT_DIR);
+    // TODO: simplify this conditional. It works for prototyping
+    if args.len() >= 2 && args[1] == "-h" || args[1] == "--help" {
+        print_git_help();
+    }
+    // Print the current crate version
+    // if args.version {
+    //     let version = option_env!("CARGO_PKG_VERSION").unwrap();
+    //     println!("gitrs version: {}", version);
+    //     return;
     // }
-
-    // match args[1].as_str() {
-    //     "init" => commands::init::main(&args[2..]),
-    //     "help" => print_full_help(),
-    //     _ => {
-    //         print_short_help();
-    //         println!("Command not support!");
-    //     }
-    // };
 }
 
-// fn print_short_help() {
-//     println!(
-//         "\
-//     This is the help menu"
-//     );
-// }
+fn print_git_help() {
+    // 11 chars to the first bracket '['
+    // TODO: there must be a better way to print this better
+    println!(
+        "
+usage: git [--version] [--help] [-C <path>] [-c <name>=<values>]
+           [-exec-path[=<path>]] [--html-path] [--man-path] [--info-path]
+           [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]
+           [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
+           [--super-prefix=<path>] [--config-env=<name>=<envvar>]
+           <command> [<args>]
 
-// fn print_full_help() {
-//     println!("");
-// }
+There are the commands that are currently implemented:
 
-// mod objects {
-//     use super::DEFAULT_GIT_DIR;
-//     use path_clean::PathClean;
-//     use std::fs;
-//     use std::io;
-//     use std::path::PathBuf;
+start a working area (see also: git help tutorial)
+   init              Create an empty Git repository or reinitialize and existing one
 
-//     // TODO: Question for future langnostic
-//     // Can I replicate this folder structure as a struct?
-//     const DEFAULT_GIT_DIR_TREE: &[&str] = &["objects", "refs", "refs/heads"];
+examine the history and state (see also: git help revision (? what the hell is that ?))
+   status            Show the working tree status
 
-//     // TODO: consider moving this to a template folder?
-//     // const DEFAULT_LOCAL_CONFIG: &str = "\
-//     // [core]
-//     // 	repositoryformatversion = 0
-//     // 	filemode = true
-//     // 	bare = false
-//     // 	logallrefupdates = true
-//     // ";
-
-//     pub struct Repository {
-//         pub path: PathBuf,
-//     }
-
-//     impl Repository {
-//         pub fn new(dir: &str) -> Self {
-//             let path = match dir {
-//                 "." | "./" => std::env::current_dir().unwrap(),
-//                 _ => PathBuf::from(dir).clean(),
-//             }
-//             .join(DEFAULT_GIT_DIR);
-
-//             // TODO: what does init reinitialize actually do?
-//             if !path.exists() {
-//                 Self::create_repo(&path).unwrap();
-//             }
-
-//             Self { path }
-//         }
-
-//         fn create_repo(git_path: &PathBuf) -> io::Result<()> {
-//             for dir in DEFAULT_GIT_DIR_TREE.iter() {
-//                 fs::create_dir_all(git_path.join(dir))?;
-//             }
-
-//             fs::write(git_path.join("HEAD"), "refs: refs/heads/main\n")?;
-//             Ok(())
-//         }
-//     }
-//     // pub struct Commit {
-//     //     parent: Option<Box<Commit>>,
-//     // }
-
-//     // struct Blob {}
-
-//     // struct Tree {}
-// }
+\"git help -a\" and \"git help -g \" list available subcommands ane some
+concept guides. See \"git help <command>\" or \"git help <concept>\"
+to read about a specific subcommands or concept.
+See \"git help git\" for an overview of the system"
+    );
+}
